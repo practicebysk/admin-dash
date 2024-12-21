ba1f5839c53bc8ce48c3f706b27493276504d014
sk <nothing225200@gmail.com>
2024-12-21 21:36:11 +0530

    change

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,16 @@
 const webpack = require('webpack');
 const dotenv = require('dotenv');
 
+// Load environment variables
 const env = dotenv.config().parsed;
 
+// Format variables for Webpack
 const envKeys = Object.keys(env).reduce((prev, next) => {
   prev[`process.env.${next}`] = JSON.stringify(env[next]);
   return prev;
 }, {});
 
 module.exports = {
-  // Ensure the correct mode (development or production) is set
-  mode: 'production',  // can also be passed during build
-
   plugins: [
     new webpack.DefinePlugin(envKeys),
   ],
